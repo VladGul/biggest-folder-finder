4865ada245429bd10eff27e4711a17cb75600b37
vladislav_guliaev <Mr.VGulyaev@gmail.com>
2022-01-24 21:32:21 +0700

    dev

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.io.File;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String folderPath = "D:\\Влад";
+        File file = new File(folderPath);
+
+        System.out.println(getFolderSize(file));
+    }
+
+    public static long getFolderSize (File folder) {
+
+        long sum = 0;
+
+        if (folder.isFile()) {
+            return folder.length();
+        }
+
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
+        }
+        return sum;
+    }
+}
